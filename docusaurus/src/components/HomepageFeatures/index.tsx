a6fcd458f6f6134d99d516808b7513f7bb46208c
Luis Camara <lcamara@vendasta.com>
2025-06-13 07:59:04 -0600

    fix: fix imgs on landing page

--- docusaurus/src/components/HomepageFeatures/index.tsx
+++ docusaurus/src/components/HomepageFeatures/index.tsx
@@ -5,14 +5,14 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Img: string;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Get Started',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Easy to Use',
+    Img: require('@site/static/img/ai-contentwriter-icon.png').default,
     description: (
       <>
         Step-by-step guidance, product overviews, and quick answers to help you get started with ease.
@@ -21,16 +21,16 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'AI-Powered Platform Documentation',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Img: require('@site/static/img/ai-receptionist-icon.png').default,
     description: (
       <>
-       Discover how Vendasta AI tools streamline your workflows from content generation to campaign automation.
+        Discover how Vendasta AI tools streamline your workflows from content generation to campaign automation.
       </>
     ),
   },
   {
     title: 'Frequently Asked Questions (FAQs)',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Img: require('@site/static/img/ai-seoexpert-icon.png').default,
     description: (
       <>
         Dive into our most commonly asked questions to get fast, clear answers about setup, troubleshooting and more.
@@ -39,11 +39,11 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Img, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={Img} className={styles.featureImage} alt="title" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
