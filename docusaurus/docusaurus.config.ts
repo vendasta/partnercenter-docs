1775cafdb9bc8fd1fe2f60320eb4234f5c6ecc80
Jesse Redl <jredl@vendasta.com>
2025-06-27 14:41:23 -0600

    fix the partner center site url

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://support.vendasta.com',
+  url: 'https://docs.vendasta.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
